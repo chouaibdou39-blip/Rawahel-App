4c8de7dadb4b58d0f787b7fb3fd0c0e946b1bfcf
chouaibdou39-blip <chouaibdou39@gmail.com>
2026-01-30 14:33:24 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // 1. إعدادات الاتصال (تم تغيير الاسم إلى sb لتجنب المشاكل)
 const SUPABASE_URL = 'https://utlbdcebtcjnjzljcaqg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bGJkY2VidGNqbmp6bGpjYXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAwMDAwMDAsImV4cCI6MjAwMDAwMDAwMH0.placeholder';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bGJkY2VidGNqbmp6bGpjYXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MjE1NTcsImV4cCI6MjA4NDk5NzU1N30.uw5pMDEQz1jUNNwktcKnb2Kflrl9JycnvCCozcDYDY0';
 
 // استخدام sb بدلاً من supabase لمنع تضارب الأسماء
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
